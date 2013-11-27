2f2f7e7093a7db05a05eac9e2e858f839509d97b
amoa400 <amoa400@gmail.com>
2013-11-27 15:20:18 +0800

    修改了一点东西

--- Public/js/home.js
+++ Public/js/home.js
@@ -11,4 +11,16 @@ $().ready(function() {
 	$('.user_menu').click(function() {
 		$('.user_menu').hide();
 	});
+	
+	// 题目预览
+	// 题目显示
+	$('.ques_preview').live('click', function() {
+		//alert($(this).attr('md_id'));
+		var id = $(this).attr('md_id');
+		if ($('.page_content').find('.ques_preview_' + id).length == 0) {
+			var div = $('<div></div>');
+			div.addClass('ques_preview_' + id);
+			div.appendTo('.page_content');
+		}
+	});
 });
